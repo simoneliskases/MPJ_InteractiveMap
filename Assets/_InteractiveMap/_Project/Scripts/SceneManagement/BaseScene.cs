097967527d9330e2928b0d4990d6ba918d764146
simoneliskases <simon.eliskases@outlook.de>
2020-11-11 14:12:48 +0100

    Added Base Scene

--- Assets/_InteractiveMap/_Project/Scripts/SceneManagement/BaseScene.cs
+++ Assets/_InteractiveMap/_Project/Scripts/SceneManagement/BaseScene.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class BaseScene : MonoBehaviour
 {
+    public GameObject layerBase;
+
     [Header("Main Scene")]
     public GameObject main;
 
@@ -18,72 +19,42 @@ public class BaseScene : MonoBehaviour
 
     public enum CurrentState    
     {
-        Main,
         MinigameBase,
         MinigameSettings,
         MinigameSelection,
         SliderBase,
     }
-    private CurrentState _state;
 
-    private string _minigameTag;
-
-    private void Start()
+    public void StateInput(CurrentState state)
     {
-        //minigameBase.gameObject.tag = _minigameTag;
-        //minigameSettings.gameObject.tag = _minigameTag;
-        //minigameSelection.gameObject.tag = _minigameTag;
-    }
-
-    public void PlayMinigame()
-    {
-        SceneManager.LoadScene(1);        
-    }
-
-    public void PlaySlider()
-    {
-        //Load Time Slider
-    }
-
-    public void Selection()
-    {
-        _state = CurrentState.MinigameSelection;
-    }
-
-    public void CloseSelection()
-    {
-        _state = CurrentState.MinigameBase;
-    }
-
-    public void Settings()
-    {
-        minigameSettings.SetActive(true);
-        minigameBase.SetActive(false);        
-        _state = CurrentState.MinigameSettings;
-    }
-    
-    public void CloseSettings()
-    {
-        minigameBase.SetActive(true);
-        minigameSettings.SetActive(false);
-        _state = CurrentState.MinigameSettings;
-    }
-
-    public void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        switch (state)
         {
-            switch (_state)
+            case CurrentState.MinigameBase:
+                ChangeState(minigameBase);
+                break;
+            case CurrentState.MinigameSettings:
+                ChangeState(minigameSettings);
+                break;
+            case CurrentState.MinigameSelection:
+                ChangeState(minigameSettings);
+                break;
+            case CurrentState.SliderBase:
+                ChangeState(sliderBase);
+                break;
+        }
+    }
+
+    private void ChangeState(GameObject layer)
+    {
+        foreach(Transform child in layerBase.transform)
+        {
+            if(child == layer)
             {
-                case CurrentState.MinigameBase:
-                    //something
-                    break;
-                case CurrentState.MinigameSettings:
-                    CloseSettings();
-                    break;
-                case CurrentState.MinigameSelection:
-                    CloseSelection();
-                    break;
+                layer.SetActive(true);
+            }
+            else if(layer != null)
+            {
+                layer.SetActive(false);
             }
         }
     }
