7ac5be6e5b02ef71a26f4b3b92dd1e6c80c7cce9
simoneliskases <simon.eliskases@outlook.de>
2020-12-19 14:53:48 +0100

    Refactored settings

--- Assets/_InteractiveMap/_Project/Scripts/SceneManagement/RestrictedSettings.cs
+++ Assets/_InteractiveMap/_Project/Scripts/SceneManagement/RestrictedSettings.cs
@@ -8,13 +8,13 @@ public class RestrictedSettings : MonoBehaviour
     public GameObject settings, password, popUp;
     public TMP_InputField passwordInput;
     public TMP_Dropdown dropdown;
+    public TextMeshProUGUI popUpMessage;
+
     private string _password = "123";
+    private int settingsID;
 
-    private void OnEnable()
+    private void Awake()
     {
-        SetActive(false, true, false);
-        passwordInput.text = "";
-
         switch (PlayerPrefs.GetFloat("minigameTime"))
         {
             case 60:
@@ -27,6 +27,12 @@ public class RestrictedSettings : MonoBehaviour
                 dropdown.value = 2;
                 break;
         }
+    }
+
+    private void OnEnable()
+    {
+        SetActive(false, true, false);
+        passwordInput.text = "";
     }
 
     private void Update()
@@ -44,40 +50,73 @@ public class RestrictedSettings : MonoBehaviour
         }
     }
 
-    public void DeleteData()
+    public void DeleteDataInput() //ID = 1
     {
         SetActive(false, false, true);
+        ChangePopUpMessage("Do you really want to delete all data ?");
+        settingsID = 1;
+    }
+
+    private void DeleteData()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+
+    public void MinigameTimeInput() //ID = 2
+    {
+        SetActive(false, false, true);
+        ChangePopUpMessage("Do you really want to change the minigame time? All highscores will be lost!");
+        settingsID = 2;
+    }
+
+    private void ChangeMinigameTime()
+    {
+        float time = 0;
+        switch (dropdown.value)
+        {
+            case 0:
+                time = 60;
+                break;
+            case 1:
+                time = 90;
+                break;
+            case 2:
+                time = 90;
+                break;
+        }
+
+        for (int i = 1; i <= 5; i++)
+        {
+            PlayerPrefs.SetString("highScoreName" + i, null);
+            PlayerPrefs.SetInt("highScorePos" + i, 0);
+        }
+
+        PlayerPrefs.SetFloat("minigameTime", time);
+    }
+
+    private void ChangePopUpMessage(string _message)
+    {
+        popUpMessage.text = _message;
     }
 
     public void Submit()
     {
-        PlayerPrefs.DeleteAll();
+        SetActive(true, false, false);
+
+        switch (settingsID)
+        {
+            case 1:
+                DeleteData();
+                break;
+            case 2:
+                ChangeMinigameTime();
+                break;
+        }
     }
 
     public void Cancel()
     {
         SetActive(true, false, false);
-    }
-
-    public void MinigameTimeInput()
-    {
-        switch (dropdown.value)
-        {
-            case 0:
-                ChangeMinigameTime(60);
-                break;
-            case 1:
-                ChangeMinigameTime(90);
-                break;
-            case 2:
-                ChangeMinigameTime(120);
-                break;
-        }
-    }
-
-    private void ChangeMinigameTime(float time)
-    {
-        PlayerPrefs.SetFloat("minigameTime", time);
     }
 
     private void OnDisable()
