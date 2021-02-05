d83c1fe273244990bd36b36651402c46ea1b013b
simoneliskases <simon.eliskases@outlook.de>
2021-02-05 21:03:19 +0100

    Various minor improvements

---
+++ Assets/_InteractiveMap/_Project/Scripts/General/SaveSystem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public static class SaveSystem
+{
+    public static void SaveScore (int score)
+    {
+        BinaryFormatter _formatter = new BinaryFormatter();
+
+        string _path = Application.persistentDataPath + "/data.im";
+        FileStream _stream = new FileStream(_path, FileMode.Create);
+    }
+}
